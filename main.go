3279c8935dedcb0c91fd7f0db067e8b23e1e42f9
Cem Beyazit Karaca Oztekin <cembkoztekin@gmail.com>
2023-04-06 01:07:47 +0300

    update deps

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"flag"
 	"net/http"
+	"net/url"
 	"os"
 
-	"github.com/bwmarrin/discordgo"
 	"github.com/google/uuid"
 	"golang.org/x/oauth2"
 )
 
+var (
+	appID = flag.String("app", os.Getenv("APPLICATION_ID"), "Application ID")
+	// token        = flag.String("token", os.Getenv("DISCORD_TOKEN"), "Application token")
+	clientSecret = flag.String("secret", os.Getenv("SECRET_KEY"), "OAuth2 secret")
+	redirectURL  = flag.String("redirect", os.Getenv("REDIRECT_URL"), "OAuth2 Redirect URL")
+)
+
 var oauthConfig = oauth2.Config{
+	ClientID:     *appID,
+	ClientSecret: *clientSecret,
 	Endpoint: oauth2.Endpoint{
 		AuthURL:  "https://discord.com/oauth2/authorize",
 		TokenURL: "https://discord.com/api/oauth2/token",
@@ -18,20 +27,18 @@ var oauthConfig = oauth2.Config{
 	Scopes: []string{"identify", "role_connections.write", "connections"},
 }
 
-var (
-	appID = flag.String("app", os.Getenv("APPLICATION_ID"), "Application ID")
-	token = flag.String("token", os.Getenv("DISCORD_TOKEN"), "Application token")
-)
-
 func init() {
 	flag.Parse()
+	// Set OAuth2 Redirect URL
+	oauthConfig.RedirectURL, _ = url.JoinPath(*redirectURL, "/callback")
 }
 
 func main() {
-	s, _ := discordgo.New("Bot " + *token)
+	// s, _ := discordgo.New("Bot " + *token)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-cache")
+		// Creating state for security
 		state := uuid.NewString()
 		cookie := http.Cookie{
 			Name:     "state",
@@ -45,30 +52,6 @@ func main() {
 		http.SetCookie(w, &cookie)
 		// Redirect the user to Discord OAuth2 page.
 		http.Redirect(w, r, oauthConfig.AuthCodeURL(state), http.StatusMovedPermanently)
-	})
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		q := r.URL.Query()
-
-		cookie, _ := r.Cookie("state")
-
-		if q["state"][0] != cookie.Value {
-			return
-		}
-
-		tokens, err := oauthConfig.Exchange(r.Context(), q["code"][0])
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		ts, _ := discordgo.New("Bearer " + tokens.AccessToken)
-
-		u, err := ts.User("@me")
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
 	})
 
 	http.ListenAndServe(":8000", nil)
